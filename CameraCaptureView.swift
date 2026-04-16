0b75be0a38bc42e41c13d363cabd3c02a68d9916
Soma101 <75684047+Soma101@users.noreply.github.com>
2026-04-15 21:58:13 -0500

    Create CameraCaptureView.swift

---
+++ CameraCaptureView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+import UIKit
+import UniformTypeIdentifiers
+
+struct CameraCaptureView: UIViewControllerRepresentable {
+    @Binding var recordedURL: URL?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        
+        // Ensure the device has a camera
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+            picker.mediaTypes = [UTType.movie.identifier] // Video only
+            picker.videoQuality = .typeHigh
+        }
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraCaptureView
+        
+        init(_ parent: CameraCaptureView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let url = info[.mediaURL] as? URL {
+                parent.recordedURL = url
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
