15c93d582f5d9ecc1c48c28a50e9d0f91fad817b
Soma101 <75684047+Soma101@users.noreply.github.com>
2026-04-21 13:05:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-record').addEventListener('click', () => {
+    window.location.href = 'camera.html';
+});
+
+document.getElementById('btn-analyze').addEventListener('click', () => {
+    window.location.href = 'analyze.html';
+});
+
+document.getElementById('btn-history').addEventListener('click', () => {
+    window.location.href = 'history.html';
+});
