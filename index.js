e6eaa84e55e9bd06cb1a2d236a83d2871c58f287
Soma101 <75684047+Soma101@users.noreply.github.com>
2026-04-29 14:01:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("KickTracker build:", "v1.1.4");
+console.log("KickTracker build:", "v1.1.5");
 
 document.getElementById('btn-record').addEventListener('click', () => {
     window.location.href = 'camera.html';
